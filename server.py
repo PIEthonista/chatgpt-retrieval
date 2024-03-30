f51b2e7ce120415286e75d11b4e82cff095fa137
jiayu.lim@uzh.ch <U2102820@siswa.um.edu.my>
2024-03-30 16:14:30 +0800

    integrated server to frontend

---
+++ server.py
@@ -0,0 +1,177 @@
+from flask import Flask, render_template, request, jsonify, redirect, session, url_for
+from flask_cors import CORS, cross_origin
+import pickle
+import joblib
+import json
+from openai import OpenAI
+from api_keys import OPENAI_API_KEY
+from main import init_record_user_expense_income_model, init_functional_model, action_layer, __DEBUGGING__, __MAX_ERROR_TRIAL__, record_user_expenses_income, expenses_prediction, data_analysis, regular_chat
+from api_keys import OPENAI_API_KEY, TAVILY_API_KEY
+from system_prompts import AUTOMATED_EXPENSE_RECORDING_SP, DATA_ANALYSIS_SP, EXPENSE_PREDICTION_SP, REGULAR_CHAT_SP, \
+                           PERSONA_GUARDIAN_SP, PERSONA_TEACHER_SP, PERSONA_ADVISOR_SP, \
+                           MODEL_POWERS_SP
+from func_call_template import MANDATORY_FUNCTION_CALL_TEMPLATE, FUNCTION_CALL_TEMPLATE
+from model_config import __MODEL__, __MAX_TOKENS__, __TEMPERATURE__, __VERBOSE__, __USER_DATA_PATH__, __MAX_DATE_RANGE__, \
+                         __DEBUGGING__, __MAX_ERROR_TRIAL__, \
+                         __PERSONA_TEACHER_AGE__, __PERSONA_GUARDIAN_AGE__, __PERSONA_ADVISOR_AGE__
+import sys
+from langchain_core.messages import HumanMessage, AIMessage
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://172.20.10.4:5000", "*"]}})
+
+def is_user(history):
+    user = history['user']
+    if user['_id'] == 1:
+        return False
+    else:
+        return True
+    
+def use_model(prompt, chat_history, user_details):
+    new_chat_history = []
+
+    if chat_history != []:
+        for history in chat_history:
+            if is_user(history):
+                new_chat_history.append(HumanMessage(content=history['text']))
+            else:
+                new_chat_history.append(AIMessage(content=history['text']))
+
+    user_name = user_details['username']
+    user_age = user_details['age']
+    user_mbti = user_details['mbti']
+    user_gender = user_details['gender']
+    user_address = user_details['address']
+    user_set_model_name = user_details['model_name']
+    
+    # preset persona
+    if user_age in range(*__PERSONA_TEACHER_AGE__):
+        persona = PERSONA_TEACHER_SP
+    elif user_age in range(*__PERSONA_GUARDIAN_AGE__):
+        persona = PERSONA_GUARDIAN_SP
+    else:
+        persona = PERSONA_ADVISOR_SP
+    
+    persona = f"Your name is {user_set_model_name}. " + persona + \
+        f" The end user's personal info is as below. Name {user_name}, Age {user_age}, MBTI {user_mbti}, \
+        Gender {user_gender} and is currently living at {user_address}. \
+        Please customise and personalise your responses for the end user based on those."
+    
+    # init required models
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    model_record_user_expense = init_record_user_expense_income_model()
+    model_regular_chat = init_functional_model(REGULAR_CHAT_SP + " " + MODEL_POWERS_SP + " " + persona)
+    model_data_analysis = init_functional_model(DATA_ANALYSIS_SP + " " + MODEL_POWERS_SP + " " + persona)
+    model_expenses_prediction = init_functional_model(EXPENSE_PREDICTION_SP + " " + MODEL_POWERS_SP + " " + persona)
+        
+    # append to chat history
+    new_chat_history.append(HumanMessage(content=prompt))
+    
+    # action layer
+    completion = action_layer(client=client, user_input=prompt)
+    # print(completion)
+    
+    funcs_to_call = {}
+    if completion.choices[0].message.tool_calls is not None:
+        for i in completion.choices[0].message.tool_calls:
+            funcs_to_call[i.function.name] = i.function.arguments
+            # funcs_to_call[i.message.function_call.name] = i.message.function_call.arguments
+                # funcs_to_call[i.message.tool_calls]
+    else:
+        funcs_to_call['regular_chat'] = ""
+        
+    # debugging
+    if __DEBUGGING__:
+        for k,v in funcs_to_call.items():
+            print(k, "-> ", end='', flush=True)
+        print()
+
+
+    # function calls must be in order
+    if 'record_user_expenses' in funcs_to_call.keys():
+        output = None
+        c = 0
+        while (output is None) and (c < __MAX_ERROR_TRIAL__):
+            output = record_user_expenses_income(agent_executor=model_record_user_expense, user_input=prompt)
+            c += 1
+        print("\n--- record_user_expenses()") if __DEBUGGING__ else None
+        print(output)
+        formatted_output = "I recorded your expense/income as follows: " + output
+        new_chat_history.append(AIMessage(content=formatted_output)) if formatted_output is not None else None
+        # must let model know that it recorded the expense
+        
+    if 'expense_prediction' in funcs_to_call.keys():
+        output = None
+        c = 0
+        while (output is None) and (c < __MAX_ERROR_TRIAL__):
+            output = expenses_prediction(client=client, agent_executor=model_expenses_prediction, chat_history=new_chat_history, user_input=prompt)
+            c += 1
+        print("\n--- expenses_prediction()") if __DEBUGGING__ else None
+        print(output)
+        if output is not None:
+            new_chat_history.append(AIMessage(content=output))
+        else:
+            new_chat_history.append(AIMessage(content=''))
+
+    if 'data_analysis' in funcs_to_call.keys():
+        output = None
+        c = 0
+        while (output is None) and (c < __MAX_ERROR_TRIAL__):
+            output = data_analysis(client=client, agent_executor=model_data_analysis, chat_history=new_chat_history, user_input=prompt)
+            c += 1
+        print("\n--- data_analysis()") if __DEBUGGING__ else None
+        print(output)
+        if output is not None:
+            new_chat_history.append(AIMessage(content=output))
+        else:
+            new_chat_history.append(AIMessage(content=''))
+
+    # final revert
+    if 'regular_chat' in funcs_to_call.keys():
+        output = None
+        c = 0
+        while (output is None) and (c < __MAX_ERROR_TRIAL__):
+            output = regular_chat(agent_executor=model_regular_chat, chat_history=new_chat_history, user_input=prompt)
+            c += 1
+        print("\n--- regular_chat()") if __DEBUGGING__ else None
+        print(output)
+        if output is not None:
+            new_chat_history.append(AIMessage(content=output))
+        else:
+            new_chat_history.append(AIMessage(content=''))
+
+    return output
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/get_user_response', methods=['POST'])
+def get_user_response():
+    user_input = request.form.get('message_to_gpt')
+    chat_history = json.loads(request.form.get('conversation_history'))
+    user_details = json.loads(request.form.get('user_details'))
+    print("DATAAAA: ",user_input)
+    updated_result = use_model(user_input, chat_history, user_details)
+    response = {'result': updated_result}
+    return jsonify(response)
+
+@app.route('/get_chat_history')
+def get_chat_history():
+    # Get the key from javascript FormData object
+    # Example in service.js of frontend
+    # const anomalyForm = new FormData();
+    # anomalyForm.append("paragraph", transcription);
+    output = request.form.get('response')
+
+@app.route('/get_data_from_database', methods=['GET'])
+def get_data_from_database():
+    # Get the key from javascript FormData object
+    # Example in service.js of frontend
+    # const anomalyForm = new FormData();
+    # anomalyForm.append("paragraph", transcription);
+    output = request.form.get('response')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
